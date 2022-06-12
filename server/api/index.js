f469cb92adf49fb7057567a3e770e33d1b2fdf76
Daniel Vaswani <vaswanidaniel@gmail.com>
2022-06-12 23:23:03 +0200

    fix vercel deploy

--- server/api/index.js
+++ server/api/index.js
@@ -7,7 +7,7 @@ const express = require("express");
 const cors = require("cors");
 // get MongoDB driver connection
 
-const PORT = 5001;
+const PORT = process.env.PORT;
 const app = express();
 
 const { MongoClient } = require("mongodb");
