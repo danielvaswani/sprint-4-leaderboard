2d0f15d8dd171b5109f2b1c1d003c4958fc0ca4c
Daniel Vaswani <vaswanidaniel@gmail.com>
2022-06-13 05:46:15 +0200

    finish Level3

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,11 +3,18 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 
 import { ref, computed } from "vue";
+import axios from "axios";
+
+// Values moved to mongoDB
+const students = ref([]);
+
+getLeaderboard();
 
 const studentsWithPlace = computed(() =>
   [...students.value]
     .sort((s1, s2) => s2.points > s1.points)
     .map((student, index) => ({
+      id: student._id,
       place: index + 1,
       name: student.name,
       points: student.points,
@@ -28,133 +35,178 @@ const sortedByCategory = computed(() =>
 const currentSortCategory = ref("place");
 const reversed = ref(false);
 
-const students = ref([
-  // {
-  //   name: "Mickey Mouse",
-  //   points: 10,
-  // },
-  // {
-  //   name: "Donald Duck",
-  //   points: 34,
-  // },
-  // {
-  //   name: "Rick Roll",
-  //   points: 15,
-  // },
-  // {
-  //   name: "Winnie the Pooh",
-  //   points: 32,
-  // },
-  // {
-  //   name: "Harley Quinn",
-  //   points: 13,
-  // },
-  // {
-  //   name: "Johnny Bravo",
-  //   points: 7,
-  // },
-  // {
-  //   name: "Cristiano Ronaldo",
-  //   points: 64,
-  // },
-  // {
-  //   name: "Weighted Companion Cube",
-  //   points: 115,
-  // },
-  // {
-  //   name: "The Huddle",
-  //   points: 34,
-  // },
-  // {
-  //   name: "Peter Pan",
-  //   points: 29,
-  // },
-  // {
-  //   name: "Dirk Gentley",
-  //   points: 28,
-  // },
-  // {
-  //   name: "Arthur Ford",
-  //   points: 61,
-  // },
-  // {
-  //   name: "Towelie",
-  //   points: 111,
-  // },
-  // {
-  //   name: "Eric Cartman",
-  //   points: 35,
-  // },
-  // {
-  //   name: "Peter Griffith",
-  //   points: 89,
-  // },
-  // {
-  //   name: "Stewie Griffith",
-  //   points: 5,
-  // },
-  // {
-  //   name: "Invader Zim",
-  //   points: 39,
-  // },
-  // {
-  //   name: "Girrrrr",
-  //   points: 48,
-  // },
-  // {
-  //   name: "Woody",
-  //   points: 44,
-  // },
-  // {
-  //   name: "Todd Sweeney",
-  //   points: 26,
-  // },
-]);
+const name = ref("");
+const points = ref(0);
+
+const currentId = ref("");
+
+const toggleSelection = (id, currentName, currentPoints) => {
+  currentId.value = currentId.value === id ? "" : id;
+  name.value = currentName;
+  points.value = currentPoints;
+};
+
+function alertAndClearText(text) {
+  alert(text);
+  name.value = "";
+  points.value = 0;
+  currentId.value = "";
+}
+
+async function addRecord() {
+  if (name.value === "") return;
+  axios
+    .post(import.meta.env.VITE_API + "/api/leaderboard/record", {
+      name: name.value,
+      points: points.value,
+    })
+    .then(
+      async () =>
+        await getLeaderboard().then(() =>
+          alertAndClearText(
+            name.value + " with points " + points.value + " added"
+          )
+        )
+    );
+}
+
+async function updateRecord() {
+  if (name.value === "") return;
+  await axios
+    .put(
+      import.meta.env.VITE_API + "/api/leaderboard/record/" + currentId.value,
+      {
+        name: name.value,
+        points: points.value,
+      }
+    )
+    .then(
+      async () =>
+        await getLeaderboard().then(() =>
+          alertAndClearText(
+            name.value + " with points " + points.value + " updated"
+          )
+        )
+    );
+}
+
+async function deleteRecord() {
+  await axios
+    .delete(
+      import.meta.env.VITE_API + "/api/leaderboard/record/" + currentId.value
+    )
+    .then(
+      async () =>
+        await getLeaderboard().then(() =>
+          alertAndClearText(name.value + " deleted")
+        )
+    );
+}
 
 // Calling my vercel hosted Mongo/Express API (MEVN)
-fetch(import.meta.env.VITE_API + "/api/leaderboard")
-  .then((response) => response.json())
-  .then((data) => students.value.push(...data));
+async function getLeaderboard() {
+  return await axios
+    .get(import.meta.env.VITE_API + "/api/leaderboard")
+    .then((response) => (students.value = [...response.data]));
+}
 </script>
 
 <template>
   <div
-    class="rounded-xl shadow-orange-700 shadow-2xl my-10 w-10/12 p-12 text-center mx-auto bg-orange-600 text-white"
+    class="flex flex-col rounded-xl shadow-orange-700 shadow-2xl mx-auto w-10/12 py-4 text-center bg-orange-600 text-white"
   >
-    <h1 class="text-4xl">Leaderboard</h1>
+    <div class="flex flex-row gap-3 py-2 justify-center w-full">
+      <input
+        type="text"
+        placeholder="Name"
+        class="px-2 w-48 rounded shadow text-black"
+        v-model="name"
+      />
+
+      <input
+        type="number"
+        placeholder="Points"
+        class="px-2 w-24 rounded shadow text-black"
+        v-model="points"
+      />
+
+      <div v-if="currentId === ''">
+        <div class="flex flex-row gap-3 w-20 justify-start">
+          <input
+            type="button"
+            value="Add"
+            class="cursor-pointer text-white px-2 py-1 rounded-md bg-blue-500 shadow-blue-600 shadow-sm"
+            @click="addRecord()"
+          />
+        </div>
+      </div>
+      <div v-else>
+        <div class="flex flex-row gap-3 w-20 justify-start">
+          <input
+            type="button"
+            value="Submit"
+            class="cursor-pointer"
+            @click="updateRecord()"
+          />
+          <input
+            type="button"
+            value="Delete"
+            class="cursor-pointer text-white px-2 py-1 rounded-md bg-red-600"
+            @click="deleteRecord()"
+          />
+        </div>
+      </div>
+    </div>
+    <h6
+      class="font-extralight py-1 mt-2 text-xs"
+      :class="
+        currentId === ''
+          ? 'opacity-80'
+          : 'text-blue-500 shadow-white drop-shadow bg-blue-200 w-48 mx-auto rounded'
+      "
+    >
+      {{
+        currentId === ""
+          ? "Select a record to edit"
+          : "Deselect record to abort"
+      }}
+    </h6>
+    <h1 class="text-4xl my-3">Leaderboard</h1>
     <div class="flex flex-row justify-around divide-blue-700 my-2 text-2xl">
-      <div
+      <h3
         class="flex-1 w-10 hover:text-orange-300 hover:cursor-pointer"
         @click="(currentSortCategory = 'place'), (reversed = !reversed)"
       >
         PLACE ⌄
-      </div>
-      <div
+      </h3>
+      <h3
         class="flex-2 w-72 hover:text-orange-300 hover:cursor-pointer text-ellipsis"
         @click="(currentSortCategory = 'name'), (reversed = !reversed)"
       >
         NAME ⌄
-      </div>
-      <div
+      </h3>
+      <h3
         class="flex-1 w-10 hover:text-orange-300 hover:cursor-pointer"
         @click="(currentSortCategory = 'points'), (reversed = !reversed)"
       >
         POINTS ⌄
-      </div>
+      </h3>
     </div>
-    <div
-      class="flex flex-row justify-around mt-3 divide-x-2 divide-orange-200"
-      v-for="student in sortedByCategory"
-      :key="student.name"
-    >
+    <div v-for="student in sortedByCategory" :key="student.name">
       <div
-        class="flex-1 w-10"
-        :class="student.place < 4 ? 'text-green-500 font-extrabold' : ''"
+        class="flex flex-row justify-around mt-3 divide-x-2 divide-orange-200 cursor-pointer"
+        :class="currentId === student.id ? 'bg-white bg-opacity-50' : ''"
+        @click="toggleSelection(student.id, student.name, student.points)"
       >
-        {{ student.place }}
+        <div
+          class="flex-1 w-10"
+          :class="student.place < 4 ? 'text-green-500 font-extrabold' : ''"
+        >
+          {{ student.place }}
+        </div>
+        <div class="flex-2 w-72 px-10 text-ellipsis">{{ student.name }}</div>
+        <div class="flex-1 w-10">{{ student.points }}</div>
       </div>
-      <div class="flex-2 w-72 px-10 text-ellipsis">{{ student.name }}</div>
-      <div class="flex-1 w-10">{{ student.points }}</div>
     </div>
   </div>
 </template>
@@ -166,7 +218,20 @@ fetch(import.meta.env.VITE_API + "/api/leaderboard")
   -moz-osx-font-smoothing: grayscale;
   box-sizing: border-box;
   padding: 0;
+  padding-top: 60px;
   margin: 0;
+  background-image: url(./assets/7130416.jpg);
+  background-size: 200vh;
+  min-height: 100vh;
+}
+
+* {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 
 @font-face {
